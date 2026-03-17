f1a6e24935255d6a3110ffaa48b15a51ce6c83d9
Guilherme Skorupa <guilherme1.skorupa@gmail.com>
2026-03-16 22:58:36 -0300

    fix: update API_URL to production in front-end/src/services/api.js and remove wrong directory

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://task-manager-api-ccfp.onrender.com';
 
 export async function getTasks() {
     const res = await fetch(`${API_URL}/tasks`);
